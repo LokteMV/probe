be004845d59ae34850b63b0c2edb31a421f15da1
Omen <Loktevmaxim96@mail.ru>
2022-04-28 08:37:03 +0300

    new_code

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://auto.ru/krasnodar/cars/mitsubishi/all/'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36', 'accept': '*/*'}
+
+def get_html(url, params = None):
+    pass
